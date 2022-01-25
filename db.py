0ed4988bc809f3aa02a3274bcfe42a6df0f7da5d
k0tyra <zhenia0072@gmail.com>
2022-01-25 14:57:01 +0200

    ok_1

--- db.py
+++ db.py
@@ -11,13 +11,14 @@ def regestration(user_id, username):
     result = db_object.fetchone()
 
     if not result:
-        db_object.execute("INSERT INTO users(id, username, message) VALUES (?, ?, ?)", (user_id, username, 0))
+        db_object.execute("INSERT INTO users(id, username, message) VALUES (%s, %s, %s)", (user_id, username, 0))
         db_connection.commit()
 
 
 def update_messages_count(user_id):
     db_object.execute(f"UPDATE users SET message = message + 1 WHERE id = {user_id}")
     db_connection.commit()
+
 
 def get_stats_messsage():
     db_object.execute("SELECT * FROM users ORDER BY message DESC LIMIT 10")
