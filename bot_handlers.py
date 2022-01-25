fc1652337a601bc02fb41193f81495c4643ab4f3
k0tyra <zhenia0072@gmail.com>
2022-01-25 02:39:21 +0200

    handlers

--- bot_handlers.py
+++ bot_handlers.py
@@ -2,6 +2,5 @@ from bot import bot
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    # username = message.from_user.username
     bot.reply_to(message, "hi!!!")
-    # bot.send_message(message, "hi!!!")
+
