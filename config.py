d1a41846505a77946e1be5ef198c5c37ab0a2eb4
k0tyra <zhenia0072@gmail.com>
2022-01-25 15:56:15 +0200

    token

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-TOKEN = '5122569079:AAFTK9Ug5ttfk2vmWCrAADvM1ZwYv4T-JZQ'
+TOKEN = '5122569079:AAEvNT6CySto1pu3cqnYMSwwz18GkuDSmyM'
 APP_NAME = 'check-datagrip'
 DB_URI = 'postgres://arxgnqftnzejui:f9149d823e6b45446b3b4bb28028da3d014d215866e8a8c417db9251d3d47aa3@ec2-176-34-105-15.eu-west-1.compute.amazonaws.com:5432/dfu8muk41dbigv'
