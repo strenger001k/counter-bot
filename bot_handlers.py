7a1d76b9e2b63f23ad992686f6ad0d305686a1ab
k0tyra <zhenia0072@gmail.com>
2022-01-25 17:22:11 +0200

    GROUP_ID

--- bot_handlers.py
+++ bot_handlers.py
@@ -2,6 +2,7 @@ from bot import bot
 from db import *
 from config import *
 
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, f"hi!!!")
@@ -11,8 +12,7 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['get_id'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, f'chat ID: {message.chat.id}\nCounter is on')
-    GROUP_ID.append(message.chat.id)
+    bot.send_message(message.chat.id, f'chat ID: {message.chat.id}')
 
 
 @bot.message_handler(commands=["stats"])
@@ -21,10 +21,8 @@ def get_stats(message):
                  get_stats_messsage(),
                  disable_web_page_preview=True,
                  parse_mode="HTML")
-    if message.chat.id in GROUP_ID:
-        update_messages_count(message.from_user.id)
 
 
-@bot.message_handler(content_types=CONTENT_TYPES, func=lambda message: message.chat.id in GROUP_ID)
+@bot.message_handler(content_types=CONTENT_TYPES, func=lambda message: message.chat.id == GROUP_ID)
 def message_from_user(message):
     update_messages_count(message.from_user.id)
