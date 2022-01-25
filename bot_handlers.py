2f8bd228f5d3d09fa90652eaa6401f98147d62e3
k0tyra <zhenia0072@gmail.com>
2022-01-25 03:44:30 +0200

    list

--- bot_handlers.py
+++ bot_handlers.py
@@ -31,15 +31,15 @@ def start(message):
 @bot.message_handler(commands=["stats"])
 def get_stats(message):
     db_object.execute("SELECT * FROM users ORDER BY messages DESC LIMIT 10")
-    result = db_object.fetchall()
+    users = db_object.fetchall()
 
-    if not result:
+    if not users:
         bot.reply_to(message, "No data...")
     else:
-        reply_message = "- Top flooders:\n"
-        for i, item in enumerate(result):
-            reply_message += f"[{i + 1}] {item[1].strip()} ({item[0]}) : {item[2]} messages.\n"
-        bot.reply_to(message, reply_message)
+        list_all_users = []
+        for user in users:
+            list_all_users.append(user[0], user[1], user[2])
+        bot.reply_to(message, ''.join(list_all_users))
 
     update_messages_count(message.from_user.id)
 
