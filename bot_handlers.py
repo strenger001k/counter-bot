93570096fe6c8e9f9d512ccddbaa375161447913
k0tyra <zhenia0072@gmail.com>
2022-01-25 03:30:40 +0200

    stats

--- bot_handlers.py
+++ bot_handlers.py
@@ -16,12 +16,6 @@ def send_welcome(message):
     update_messages_count(user_id, username)
 
 
-@bot.message_handler(content_types=["text"])
-def message_from_user(message):
-    user_id = message.from_user.id
-    update_messages_count(user_id)
-
-
 @bot.message_handler(commands=["stats"])
 def get_stats(message):
     db_object.execute("SELECT * FROM users ORDER BY messages DESC LIMIT 10")
@@ -33,7 +27,13 @@ def get_stats(message):
         reply_message = "- Top flooders:\n"
         for item in enumerate(result):
             reply_message += f"{item[1].strip()} ({item[0]}) : {item[2]} messages.\n"
-    bot.reply_to(message, reply_message)
+        bot.reply_to(message, reply_message)
 
+    user_id = message.from_user.id
+    update_messages_count(user_id)
+
+
+@bot.message_handler(func=lambda message: True, content_types=["text"])
+def message_from_user(message):
     user_id = message.from_user.id
     update_messages_count(user_id)
