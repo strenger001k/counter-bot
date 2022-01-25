b31414ac5e0e87bfabed48e9a44980e005fb027d
k0tyra <zhenia0072@gmail.com>
2022-01-25 17:51:37 +0200

    group

--- db.py
+++ db.py
@@ -5,14 +5,14 @@ db_connection = psycopg2.connect(DB_URI, sslmode="require")
 db_object = db_connection.cursor()
 
 
-def regestration(user_id, username):
+def regestration(user_id, username, group_id):
     db_object.execute(f"SELECT id FROM users WHERE id = {user_id}")
     result = db_object.fetchone()
 
     if not result:
-        db_object.execute("INSERT INTO users(id, username, message)"
-                          "VALUES (%s, %s, %s)",
-                          (user_id, username, 0))
+        db_object.execute("INSERT INTO users(id, username, message, group)"
+                          "VALUES (%s, %s, %s, %s)",
+                          (user_id, username, 0, group_id))
         db_connection.commit()
 
 
