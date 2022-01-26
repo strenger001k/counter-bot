dc19c6810b3afab3372bf287d9ff9a1e3d2233e6
k0tyra <zhenia0072@gmail.com>
2022-01-27 00:02:21 +0200

    more check in adding to db

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 TOKEN = '5122569079:AAEvNT6CySto1pu3cqnYMSwwz18GkuDSmyM'
-GROUP_ID = -1001691283602
 APP_NAME = 'check-datagrip'
 DB_URI = 'postgres://krahwddydehepx:be210ba7743fc23d3ba05b26d40e3ebbacf013922b9c1fea275bb62999a7a2e1@ec2-52-208-145-55.eu-west-1.compute.amazonaws.com:5432/darqdhm1m2m1lv'
 CONTENT_TYPES = ["text",
--- db.py
+++ db.py
@@ -1,16 +1,21 @@
 import psycopg2
 from config import DB_URI
 
-
 db_connection = psycopg2.connect(DB_URI)
 db_object = db_connection.cursor()
 
 
+def repea_group(users, group):
+    for user in users:
+        if user[1].strip() == group:
+            return True
+
+
 def regestration(user_id, username, group):
-    db_object.execute(f"SELECT * FROM users WHERE id = {user_id} ORDER BY ID DESC")
-    id_user = db_object.fetchone()
+    db_object.execute(f"SELECT * FROM users WHERE id = {user_id}")
+    id_user = db_object.fetchall()
     if id_user:
-        if id_user[1].strip() != group:
+        if not repea_group(id_user, group):
             db_object.execute("INSERT INTO users (id, group_id, username) VALUES (%s, %s, %s)", (user_id, group, username))
             db_connection.commit()
     else:
