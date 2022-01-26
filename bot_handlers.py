0b0b652f3e4f9cf3cf8ee4162bc95af6be3cbe52
k0tyra <zhenia0072@gmail.com>
2022-01-26 20:47:56 +0200

    str()

--- bot_handlers.py
+++ bot_handlers.py
@@ -6,17 +6,17 @@ from config import *
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, f"hi!!!")
-    regestration(message.from_user.id, message.from_user.username, message.chat.id)
+    regestration(message.from_user.id, message.from_user.username, str(message.chat.id))
 
 
 @bot.message_handler(commands=["stats"])
 def get_stats(message):
     bot.reply_to(message,
-                 get_stats_messsage(message.chat.id),
+                 get_stats_messsage(str(message.chat.id)),
                  disable_web_page_preview=True,
                  parse_mode="HTML")
 
 
 @bot.message_handler(content_types=CONTENT_TYPES)
 def message_from_user(message):
-    update_messages_count(message.from_user.id, message.chat.id)
+    update_messages_count(message.from_user.id, str(message.chat.id))
